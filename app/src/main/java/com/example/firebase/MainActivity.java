aa0cf8fd653017b8aa7d38eb203bd8e9de1b4d69
EdemLinzi <edemlinzi11@gmail.com>
2019-03-13 10:48:10 +0100

    CompletionHandler added

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.firebase;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -83,7 +87,15 @@ public class MainActivity extends AppCompatActivity {
         Details details = new Details(name,description,email);
 
         databaseReference.push()
-                .setValue(details);
+                .setValue(details, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                        if(databaseError != null){
+                            Toast.makeText(MainActivity.this, "Not Saved",Toast.LENGTH_LONG).show();                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Saved",Toast.LENGTH_LONG).show();                        }
+                    }
+                });
     }
 
     public void openActivity2(View v){
